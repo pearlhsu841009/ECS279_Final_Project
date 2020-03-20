08f5150047d87c9b0a040fee67696622c3893d2b
Zixin Chi <czx_530@126.com>
2020-03-20 09:23:21 -0700

    character controller and rigid body

---
+++ CharacterController-RigidBody/Assets/Scritps/RigidbodyCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+
+    public float Speed = 5f;
+    public float JumpHeight = 2f;
+    public float GroundDistance = 0.2f;
+    public float DashDistance = 5f;
+    public LayerMask Ground;
+    private Rigidbody rb;
+    private Vector3 _inputs = Vector3.zero;
+    private bool isGround = true;
+    private Transform _groundChecker;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _groundChecker = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        isGround = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        _inputs = Vector3.zero;
+        _inputs.x = Input.GetAxis("Horizontal");
+        _inputs.z = Input.GetAxis("Vertical");
+        if (_inputs != Vector3.zero)
+            transform.forward = _inputs;
+
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime)));
+            rb.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _inputs * Speed * Time.fixedDeltaTime);
+    }
+}
